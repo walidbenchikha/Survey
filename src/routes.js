25d340d27a8aeaac392bb134837d8add069e786c
Walid Ben Chikha <walid.ben.chikha01@gmail.com>
2019-03-06 17:23:54 -0600

    requireAuth function and skipAuth

--- src/routes.js
+++ src/routes.js
@@ -5,14 +5,46 @@ import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import LogoutPage from './pages/LogoutPage';
 import UserSurveysPage from './pages/UserSurveysPage';
+import { home } from './reducers/session';
+import { fetchCurrentUserRequest, fetchCurrentUserRequestSuccess, fetchCurrentUserRequestFail } from './actions/login';
+
+const skipAuthPaths = ['/login', '/register', '/logout'];
 
 export default function routes(store) {
+    function requireAuth(nextState, replace, next) {
+      const state = store.getState();
+      const nextPath = nextState.location.pathname;
+      console.log(nextPath);
+      if (!state.session.user && skipAuthPaths.indexOf(nextPath) === -1) {
+        store.dispatch(fetchCurrentUserRequest()).then(res => {
+          store.dispatch(fetchCurrentUserRequestSuccess(res));
+          dispatchHomePage(nextState, replace, store);
+          next();
+        }, err => {
+          store.dispatch(fetchCurrentUserRequestFail(nextPath));
+          replace('/login');
+          next();
+        });
+      } else {
+        dispatchHomePage(nextState, replace, store);
+        next();
+      }
+    }
+  
+    function dispatchHomePage(nextState, replace, store) {
+      const state = store.getState();
+      if (state.session.user && nextState.location.pathname === '/') {
+        replace({
+          pathname: home(state.session.user)
+        });
+      }
+    }
     return (
-        <Route path="/" component={App}>
+        <Route path="/" component={App} onEnter={requireAuth}>
             <Route path="register" component={RegisterPage}/>
             <Route path="login" component={LoginPage}/>
             <Route path="logout" component={LogoutPage}/>
-            <Route path="user/surveys" component={UserSurveysPage}/>
+            <Route path="user/surveys" component={UserSurveysPage} onEnter={requireAuth}/>
         </Route>
     );
   }
